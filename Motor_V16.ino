d9517e6282708b7cf1e979f3a6118a2275def814
paoromero4 <promero1705@gmail.com>
2025-08-03 22:53:35 +0200

    combining branches

--- Motor_V16.ino
+++ Motor_V16.ino
@@ -21,8 +21,8 @@ int speedR = 0;
 
 // time stamps
 const int beg_rumba = 12700; //12500
-const int end_rumba = 18650; 
-const int beg_corner = 21300; 
+const int end_rumba = 18650;
+const int beg_corner = 21300;
 
 
 Timer timer;
@@ -33,7 +33,7 @@ void setup()  {
   pinMode(L_sens, INPUT);
   pinMode(M_sens, INPUT);
   pinMode(R_sens, INPUT);
-  
+
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(BIN1, OUTPUT);
@@ -47,7 +47,7 @@ void setup()  {
 }
 
 void loop(){
-  
+
   int L_sensVal;
   int M_sensVal;
   int R_sensVal;
@@ -58,22 +58,22 @@ void loop(){
     R_sensVal = map(analogRead(R_sens), 0, 1000, 0, 100);
     if (L_sensVal < (R_sensVal-7)) {
       speedL = 230; //  (140s, 160t, 50t), (160s, 170t, 50t), (200, 185, 50), (250s,200t, 50t), (230s, 230t, 60t)
-      speedR = 60; 
+      speedR = 60;
     }
-      
+
     if (R_sensVal < (L_sensVal-7)) { //7
-      speedR = 230; 
+      speedR = 230;
       speedL = 60;
     }
 
     if (L_sensVal < (R_sensVal+7) && L_sensVal > (R_sensVal-7)) {
       speedL = 225; //230,232
-      speedR = 230; 
+      speedR = 230;
     }
-    
-    drive(speedL, speedR);      
+
+    drive(speedL, speedR);
   }
-  
+
 
   while (timer.read() > beg_rumba && timer.read() < end_rumba){ // RUMBA while in the curve, activate slow drive, between 27 to 40 secs
 
@@ -85,15 +85,15 @@ void loop(){
       speedL = 130; //(140,50)(120,70)
       speedR = 65;
     }
-    else if (L_sensVal < (R_sensVal-10)) { // big adjustment, left is off track 
+    else if (L_sensVal < (R_sensVal-10)) { // big adjustment, left is off track
       speedL = 225; //(225,-115)
       speedR = -115;
     }
-      
+
     if (R_sensVal < (L_sensVal-7)&& R_sensVal > (L_sensVal-10)) { // small adjustment, right is off
       speedR = 130;
       speedL = 65;
-    } 
+    }
     else if (R_sensVal < (L_sensVal-10)) { // big adjustment, right is off
       speedR = 225;
       speedL = -115;
@@ -103,20 +103,20 @@ void loop(){
       speedL = 70;
       speedR = 82;
     }
-    
-    drive(speedL, speedR);    
-  }     
+
+    drive(speedL, speedR);
+  }
 
   while (timer.read() > end_rumba && timer.read() < beg_corner){ //Past the curve, activate normal drive. gaps
     L_sensVal = map(analogRead(L_sens), 0, 1000, 0, 100);
     M_sensVal = map(analogRead(M_sens), 0, 1000, 0, 100);
     R_sensVal = map(analogRead(R_sens), 0, 1000, 0, 100);
-    
+
     if (L_sensVal < (R_sensVal-7)) {
       speedL = 155;
       speedR = 55;
     }
-      
+
     if (R_sensVal < (L_sensVal-7)) {
       speedR = 155;
       speedL = 55;
@@ -126,8 +126,8 @@ void loop(){
       speedL = 130; //120,  130
       speedR = 142; //122
     }
-    
-    drive(speedL, speedR);      
+
+    drive(speedL, speedR);
   }
 
 
@@ -140,13 +140,13 @@ void loop(){
       speedL = 140;
       speedR = 50;
     }
-  
-      
+
+
     if (R_sensVal < (L_sensVal-7)) { // small adjustment, right is off
       speedR = 180;
       speedL = 50;
-    } 
-   
+    }
+
 
     if (M_sensVal > 55) {
       speedL = 90; //70
@@ -157,11 +157,11 @@ void loop(){
       speedL = 145; //70,70, 110  130
       speedR = 145;
     }
-    
-    drive(speedL, speedR);  
+
+    drive(speedL, speedR);
 
     if (M_sensVal < 40 && L_sensVal < 40 && R_sensVal < 40){ //Barbline is off track
-    uint32_t star_time = timer.read(); 
+    uint32_t star_time = timer.read();
     while((timer.read()-star_time) < 250){ //turn left for 350 ms, check for path along the way.
         L_sensVal = map(analogRead(L_sens), 0, 1000, 0, 100);
         M_sensVal = map(analogRead(M_sens), 0, 1000, 0, 100);
@@ -174,16 +174,16 @@ void loop(){
     while (M_sensVal < 40){
       M_sensVal = map(analogRead(M_sens), 0, 1000, 0, 100);
       drive(200, -200); //80,-80 120 150
-        
-    }  
-    }     
+
+    }
+    }
   }
 }
 
 //----------------------------------------------
 void motorWrite(int motorSpeed, int xIN1, int xIN2, int PWMx)
 {
- 
+
   if (motorSpeed > 0)          // it's forward
   {  digitalWrite(xIN1, LOW);
      digitalWrite(xIN2, HIGH);
@@ -191,7 +191,7 @@ void motorWrite(int motorSpeed, int xIN1, int xIN2, int PWMx)
   else                         // it's reverse
   {  digitalWrite(xIN1, HIGH);
      digitalWrite(xIN2, LOW);
-  } 
+  }
 
     motorSpeed = abs(motorSpeed);
     motorSpeed = constrain(motorSpeed, 0, 255);   // Just in case...
